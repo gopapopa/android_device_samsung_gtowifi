efd84dee9aead41933702315b40357eca6bcb650
gopapopa <technoscratch.official@gmail.com>
2026-09-24 20:35:25 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_gtowifi.mk
+    $(LOCAL_DIR)/twrp_gtowifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_gtowifi-user \
-    omni_gtowifi-userdebug \
-    omni_gtowifi-eng
+    twrp_gtowifi-eng \
+    twrp_gtowifi-userdebug
